249ec5cad03293e859dc61290838cc0e73830993
coder-utshab <ghoshutshob470@gmail.com>
2026-01-13 12:50:34 +0530

    bt

---
+++ events.js
@@ -0,0 +1,45 @@
+// console.log('This is separateJS file');
+
+//option- 1 : directly set on the HTML Element
+
+
+//option 2: add onclick function
+function makeRed(){
+            document.body.style.backgroundColor = 'red';
+        }
+
+       //option 3: 
+        const makeBlueButton = document.getElementById('make-blue');
+        makeBlueButton.onclick = makeBlue;
+
+        function makeBlue(){
+            document.body.style.backgroundColor = 'blue';
+        }
+
+
+        //option-3 another
+        const purpleButton = document.getElementById('make-purple');
+       purpleButton.onclick = function makePurple() {
+        document.body.style.backgroundColor = 'purple';
+       }
+
+
+// option-4:----
+  const pinkButton = document.getElementById('make-pink');
+        pinkButton.addEventListener('click', makePink);
+
+        function makePink(){
+            document.body.style.backgroundColor = 'pink'
+        }
+
+        //option- 4 another function
+        const makeGreenButton = document.getElementById('make-green');
+  makeGreenButton.addEventListener('click', function(){
+    document.body.style.backgroundColor = 'green';
+  });
+
+
+  //option -- 4 finall option
+   document.getElementById('make-goldenrod').addEventListener('click',function(){
+    document.body.style.backgroundColor = 'goldenrod';
+  })
